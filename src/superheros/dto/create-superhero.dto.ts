9eb59e9db85ff59e4702651794ebf39bb16c6708
Mubashir Iqbal <mubashir@menlo79.com>
2021-08-07 16:03:40 +0200

    added create dto

--- src/superheros/dto/create-superhero.dto.ts
+++ src/superheros/dto/create-superhero.dto.ts
@@ -1,1 +1,30 @@
-export class CreateSuperheroDto {}
+import { IsArray, IsNotEmpty, IsString, ValidateNested } from 'class-validator';
+
+export class CreateSuperheroDto {
+  @IsNotEmpty()
+  @IsString()
+  alias: string;
+
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsArray()
+  @ValidateNested({ each: true })
+  @IsString()
+  powers: string[];
+
+  @IsArray()
+  @ValidateNested({ each: true })
+  @IsString()
+  weapons: string[];
+
+  @IsNotEmpty()
+  @IsString()
+  origin: string;
+
+  @IsArray()
+  @ValidateNested({ each: true })
+  @IsString()
+  associations: string[];
+}
