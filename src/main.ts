d7a7235e7132265d28ae95b71e8652f786c2debf
Mubashir Iqbal <mubashir@menlo79.com>
2021-08-09 01:32:02 +0200

    added API docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('API Docs')
+    .setDescription('API Description')
+    .setVersion('1.0.0')
+    .build();
+  const swaggerDocument = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, swaggerDocument);
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
--- src/superheros/superheros.controller.ts
+++ src/superheros/superheros.controller.ts
@@ -10,27 +10,41 @@ import {
 import { SuperherosService } from './superheros.service';
 import { CreateSuperheroDto } from './dto/create-superhero.dto';
 import { UpdateSuperheroDto } from './dto/update-superhero.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Superheros')
 @Controller('superheros')
 export class SuperherosController {
   constructor(private readonly superherosService: SuperherosService) {}
 
   @Post()
+  @ApiOperation({
+    summary: 'Create a super hero',
+  })
   create(@Body() createSuperheroDto: CreateSuperheroDto) {
     return this.superherosService.create(createSuperheroDto);
   }
 
   @Get()
+  @ApiOperation({
+    summary: 'Get all super heros',
+  })
   findAll() {
     return this.superherosService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({
+    summary: 'Get a super hero',
+  })
   findOne(@Param('id') id: string) {
     return this.superherosService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiOperation({
+    summary: 'Update a super hero',
+  })
   update(
     @Param('id') id: string,
     @Body() updateSuperheroDto: UpdateSuperheroDto,
@@ -39,6 +53,9 @@ export class SuperherosController {
   }
 
   @Delete(':id')
+  @ApiOperation({
+    summary: 'Delete a super hero',
+  })
   remove(@Param('id') id: string) {
     return this.superherosService.remove(id);
   }
